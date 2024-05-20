217ffdf29b7e87cf16df0cd2ddd9969fe4d996aa
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2024-05-20 23:45:30 +0300

    added LoginFragment

--- presentation/auth/src/main/java/com/example/auth/entry/EntryFragment.kt
+++ presentation/auth/src/main/java/com/example/auth/entry/EntryFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.auth.R
 import com.example.auth.databinding.FragmentEntryBinding
 
 
@@ -19,6 +21,17 @@ class EntryFragment : Fragment() {
     ): View {
         _binding = FragmentEntryBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.run {
+            buttonLoginEntry.setOnClickListener {
+                findNavController().navigate(R.id.action_entryFragment_to_loginFragment)
+            }
+        }
+
     }
     
     override fun onDestroyView() {
