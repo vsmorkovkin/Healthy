745afe709decfe8e590a8fa4892a1475c05baa23
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2024-05-20 19:10:52 +0300

    added auth module: AuthorizationActivity with WelcomeFragment

--- app/src/main/java/com/example/healthy/StartingActivity.kt
+++ app/src/main/java/com/example/healthy/StartingActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.auth.AuthorizationActivity
 
 class StartingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,7 @@ class StartingActivity : Activity() {
 
         setContentView(R.layout.activity_starting)
 
-        val intent = Intent(baseContext, MainActivity::class.java)
+        val intent = Intent(baseContext, AuthorizationActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
     }
--- presentation/auth/src/main/java/com/example/auth/AuthorizationActivity.kt
+++ presentation/auth/src/main/java/com/example/auth/AuthorizationActivity.kt
@@ -1,11 +1,11 @@
-package com.example.healthy
+package com.example.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authorization)
     }
 }
